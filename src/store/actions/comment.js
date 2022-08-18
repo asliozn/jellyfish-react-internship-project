866420318cd21e0ddfe9f9225c52e822cfe69300
Aslı Özen <asliiozen@hotmail.com>
2022-08-18 18:00:23 +0300

    Added for comment operations

--- src/store/actions/comment.js
+++ src/store/actions/comment.js
@@ -15,3 +15,39 @@ export const fetchCommentsBySlug = (slug) => async (dispatch) => {
         console.log(error);
     }
 }
+
+export const addComment = (slug, comment) => async (dispatch) => {
+
+    const user = JSON.parse(localStorage.getItem('user'));
+    const config = {
+        headers: { Authorization: `Bearer ${user.token}`}
+      }
+    try {
+        const res = await axios.post(`https://api.realworld.io/api/articles/${slug}/comments`, comment,config);
+        const data = await res.data;
+        console.log(data);
+        dispatch({
+            type: types.ADD_COMMENT,
+            payload: data
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const deleteComment = (slug, id) => async (dispatch) => {
+
+    try {
+        const res = await axios.delete(`https://api.realworld.io/api/articles/${slug}/comments/${id}`);
+        const data = await res.data;
+        console.log(data);
+        dispatch({
+            type: types.DELETE_COMMENT,
+            payload: data
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
--- src/store/reducers/commentReducer.js
+++ src/store/reducers/commentReducer.js
@@ -24,6 +24,17 @@ export default function commentReducer(state = initialState, action) {
                 ...state,
                 comments: action.payload
             };
+
+        case types.ADD_COMMENT:
+            return {
+                ...state,
+                comments: [ ...state.comments, action.payload ]
+            };
+        case types.DELETE_COMMENT:
+            return {
+                ...state,
+                comments: state.comments.filter(comment => comment.id !== action.payload)
+            };
         default:
             return {...state,};
     }
