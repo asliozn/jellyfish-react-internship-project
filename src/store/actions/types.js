090cf9d03f90ff9c60e81da4eaffe904afac5b4d
Aslı Özen <asliiozen@hotmail.com>
2022-08-18 17:59:06 +0300

    added new types

--- src/store/actions/types.js
+++ src/store/actions/types.js
@@ -1,6 +1,16 @@
 export const FETCH_POSTS = "FETCH_POSTS";
 export const CREATE_ARTICLE = "CREATE_ARTICLE";
+
 export const FETCH_TAGS = "FETCH_TAGS";
 export const FETCH_ARTICLE = "FETCH_ARTICLE";
+
 export const FETCH_COMMENTS_BY_SLUG = "FETCH_COMMENTS_BY_SLUG";
+export const ADD_COMMENT = "ADD_COMMENT";
+export const DELETE_COMMENT = "DELETE_COMMENT";
+
+export const LIKE_ARTICLE = "LIKE_ARTICLE";
+export const GET_PROFILE = "GET_PROFILE";
+
+export const GET_CURRENT_USER = "GET_CURRENT_USER";
+export const GET_ARTICLES_BY_AUTHOR = "GET_ARTICLES_BY_AUTHOR";
 
