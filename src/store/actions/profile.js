da5de6dff494ffe8d9355270047e653cc7804dff
Aslı Özen <asliiozen@hotmail.com>
2022-08-22 17:39:10 +0300

    Added follow user func

--- src/store/actions/profile.js
+++ src/store/actions/profile.js
@@ -16,3 +16,46 @@ export const getProfile = (username) => async (dispatch) =>{
     }
 }
 
+
+export const followUser = (username) => async (dispatch) =>{
+
+    
+    
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    const config = {
+        headers: { Authorization: `Bearer ${user.token}`,}
+    }   
+    console.log(user.token);
+    try {
+            const res = await axios.post(`https://api.realworld.io/api/profiles/${username}/follow`, config);
+            const data = res.data;
+                dispatch({  
+                    type: types.FOLLOW_USER,
+                    payload: data
+                });
+        
+        } catch (error) {
+                console.log(error);
+        }
+}
+export const unfollowUser = (username) => async (dispatch) =>{
+        
+            const user = JSON.parse(localStorage.getItem('user'));
+            const config = {
+                headers: { Authorization: `Bearer ${user.token}`,}
+            }
+            try {
+                    const res = await axios.delete(`https://api.realworld.io/api/profiles/${username}/follow`, config);
+                    const data = res.data;
+                        dispatch({  
+                            type: types.UNFOLLOW_USER,
+                            payload: data
+                        });
+                
+                } catch (error) {
+                        console.log(error);
+                }
+        }
+
+
